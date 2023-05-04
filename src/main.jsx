05abee8f269b324ab7469d02260c05c090b3e310
Juan Cuevas <juanandrescuevas14@gmail.com>
2023-05-04 11:49:58 -0400

    Agregando tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,8 @@ import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     </React.StrictMode>,
 )
